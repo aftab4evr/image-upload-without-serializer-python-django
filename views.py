8a2cd800ecdab8068e675572909d03b53de3deff
Aftab Hussain <49341815+aftab4evr@users.noreply.github.com>
2019-12-06 20:47:36 +0530

    Update views.py

--- views.py
+++ views.py
@@ -12,3 +12,19 @@ class ImageView(APIView):
         path = default_storage.save('tempimage/'+str(file_name)+'.jpeg', ContentFile(data.read()))
         message = "http://" + str(get_current_site(request))+'/media/'+path
         return Response({"file_name":message})
+
+#Upload Image in base64
+class Base64ImageView(APIView):
+    """
+    upload image using base64
+    {
+    "image":"bas6cod"
+    }
+    """
+    def post(self,request):
+        image = request.data['image']
+        file_name = random.randint(00000,99999)
+        with open("media/tempimage/"+str(file_name)+".png", "wb") as fh:
+            fh.write(base64.decodebytes(image.encode()))
+        message = "http://" + str(get_current_site(request))+'/media/tempimage/'+str(file_name)+".png"
+        return Response({"file_name":message})
